ed544d103d4951b158fcc5cf5cbb9a0904f527b7
Jacob <jake.gallegos2047@gmail.com>
2026-03-25 01:55:56 -0400

    Update NoodlesAutomation.js
    Updating base script into a smart loop for future usage

--- NoodlesAutomation.js
+++ NoodlesAutomation.js
@@ -1,6 +1,18 @@
-/** @param {NS} ns */
 export async function main(ns) {
+  const target = ns.args[0];
+
   while (true) {
-    await ns.hack("n00dles");
+    const money = ns.getServerMoneyAvailable(target);
+    const maxMoney = ns.getServerMaxMoney(target);
+    const security = ns.getServerSecurityLevel(target);
+    const minSecurity = ns.getServerMinSecurityLevel(target);
+
+    if (security > minSecurity + 5) {
+      await ns.weaken(target);
+    } else if (money < maxMoney * 0.75) {
+      await ns.grow(target);
+    } else {
+      await ns.hack(target);
+    }
   }
 }
